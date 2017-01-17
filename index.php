bd548dab3951ac4fa5496a03bc8bf4bfba9bca39
Adem <ademtrkr@gmail.com>
2017-01-18 01:27:11 +0300

    scss ve genel tasasım

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:r%7CNoticia+Text:r,i,b,bi&amp;subset=latin,latin-ext,latin,latin-ext" media="all">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <style>
+        body {
+            padding-top: 50px;
+        }
+
+        .starter-template {
+            padding: 40px 15px;
+            text-align: center;
+        }
+    </style>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="navbar navbar-default navbar-fixed-top " role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand logo" href="#"></a>
+        </div>
+        <div class="collapse navbar-collapse ">
+            <ul class="nav navbar-nav navbar-right navbar-nav-ustmenu">
+                <li class="active"><a href="#">Ana Sayfa</a></li>
+                <li><a href="#about">Hakkımda</a></li>
+                <li><a href="#contact">Portföy</a></li>
+                <li><a href="#contact">Blog</a></li>
+                <li><a href="#contact">İletişim</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+
+<div class="page" id="hakkimda">
+ <h2>Hakkımdai</h2>
+    <hr>
+<div class="container">
+        <div class="col-xs-12 col-md-4"><img class="col-xs-12 col-md-12" src="images/avatar.png" >
+            <hr style="width: 100%;margin-left:inherit;margin-top:15px;">
+        </div>
+        <div class="col-xs-12 col-md-8"> <h3>Ben Kimim? </h3>
+        <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt fermentum euismod. Aliquam molestie pulvinar mi, eget aliquam purus tristique eu. Aenean vel neque tincidunt, molestie magna a, commodo neque. Suspendisse pellentesque dui eros, non venenatis eros ultrices at. Sed convallis ex velit, at volutpat leo facilisis sed. Integer gravida, nunc vel accumsan ornare, nisi magna suscipit justo, sed pellentesque sem elit venenatis odio. Quisque tincidunt ultricies tempor. Aliquam eu urna eu urna sollicitudin rhoncus. Nunc malesuada nulla lectus, vitae faucibus dui dignissim ut. Duis id interdum dolor. Nulla non lacus venenatis, dapibus augue ut, feugiat dui. Aliquam eleifend arcu non magna sodales hendrerit.</p>
+            <p>Donec eget augue lorem. Quisque in maximus erat. Donec non quam orci. Nam vel elit pellentesque, varius turpis eu, malesuada odio. Maecenas gravida velit sit amet purus sollicitudin tempor id nec ligula. Cras tempus lorem non sodales interdum. Nam ornare at nibh ac lobortis. Proin laoreet euismod semper. Mauris id congue est. Nulla id tortor eget quam dictum blandit nec pulvinar ex. Maecenas est libero, malesuada eu hendrerit vel, pulvinar et urna. Curabitur tempus condimentum arcu id congue. Fusce gravida ex nisl, eu elementum ipsum porttitor vel. Integer pretium scelerisque eros, sed lacinia turpis pharetra eu. Praesent vitae egestas ipsum. Etiam dui elit, maximus non tristique ac, pellentesque sed urna.</p>
+
+
+            <hr style="width: 100%;margin:inherit;">
+        </div>
+
+
+</div>
+    <div class="container-fluid">
+
+        <div class="islerbox">
+
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle " src="images/avatar3.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle" src="images/avatar3.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+        </div>
+    </div>
+
+</div><!-- /.container -->
+
+<div class="page" id="anasayfa">
+
+    <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a
+            mostly barebones HTML document.</p>
+    </div>
+
+</div><!-- /.container -->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
