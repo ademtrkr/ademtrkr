77d5e8ea3ea7673f26d687b076c7d0f88572aec0
Adem <ademtrkr@gmail.com>
2017-01-18 03:51:18 +0300

    düzenlemeler

---
+++ include/navbar.php
@@ -0,0 +1,22 @@
+<div class="navbar navbar-default navbar-fixed-top " role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand logo" href="#"></a>
+        </div>
+        <div class="collapse navbar-collapse ">
+            <ul class="nav navbar-nav navbar-right navbar-nav-ustmenu">
+                <li class="active"><a href="#">Ana Sayfa</a></li>
+                <li><a href="#about">Hakkımda</a></li>
+                <li><a href="#contact">Portföy</a></li>
+                <li><a href="#contact">Blog</a></li>
+                <li><a href="#contact">İletişim</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
---
+++ sayfalar/hakkimda.php
@@ -0,0 +1,258 @@
+<div class="page" id="hakkimda">
+    <h2>Hakkımda</h2>
+    <hr>
+    <div class="container">
+        <div  class="col-xs-12 col-md-4">
+            <img style="display: none;" class="col-xs-12 col-md-12" src="images/avatar.png" >
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+
+                        <div class="card hovercard">
+                            <div class="cardheader">
+
+                            </div>
+                            <div class="avatar">
+                                <img alt="" src="images/avatar.png">
+                            </div>
+                            <div class="info">
+                                <div class="title">
+                                    <a target="_blank" href="http://scripteden.com/">Script Eden</a>
+                                </div>
+                                <div class="desc">Passionate designer</div>
+                                <div class="desc">Curious developer</div>
+                                <div class="desc">Tech geek</div>
+                            </div>
+                            <div class="bottom">
+                                <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/webmaniac">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a class="btn btn-danger btn-sm" rel="publisher"
+                                   href="https://plus.google.com/+ahmshahnuralam">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                                <a class="btn btn-primary btn-sm" rel="publisher"
+                                   href="https://plus.google.com/shahnuralam">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a class="btn btn-warning btn-sm" rel="publisher" href="https://plus.google.com/shahnuralam">
+                                    <i class="fa fa-behance"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <!---------->
+            <hr style="width: 100%;margin-left:inherit;margin-top:15px;">
+        </div>
+        <div class="col-xs-12 col-md-8"> <h3>Ben Kimim? </h3>
+            <br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt fermentum euismod. Aliquam molestie pulvinar mi, eget aliquam purus tristique eu. Aenean vel neque tincidunt, molestie magna a, commodo neque. Suspendisse pellentesque dui eros, non venenatis eros ultrices at. Sed convallis ex velit, at volutpat leo facilisis sed. Integer gravida, nunc vel accumsan ornare, nisi magna suscipit justo, sed pellentesque sem elit venenatis odio. Quisque tincidunt ultricies tempor. Aliquam eu urna eu urna sollicitudin rhoncus. Nunc malesuada nulla lectus, vitae faucibus dui dignissim ut. Duis id interdum dolor. Nulla non lacus venenatis, dapibus augue ut, feugiat dui. Aliquam eleifend arcu non magna sodales hendrerit.</p>
+            <p>Donec eget augue lorem. Quisque in maximus erat. Donec non quam orci. Nam vel elit pellentesque, varius turpis eu, malesuada odio. Maecenas gravida velit sit amet purus sollicitudin tempor id nec ligula. Cras tempus lorem non sodales interdum. Nam ornare at nibh ac lobortis. Proin laoreet euismod semper. Mauris id congue est. Nulla id tortor eget quam dictum blandit nec pulvinar ex. Maecenas est libero, malesuada eu hendrerit vel, pulvinar et urna. Curabitur tempus condimentum arcu id congue. Fusce gravida ex nisl, eu elementum ipsum porttitor vel. Integer pretium scelerisque eros, sed lacinia turpis pharetra eu. Praesent vitae egestas ipsum. Etiam dui elit, maximus non tristique ac, pellentesque sed urna.</p>
+
+
+
+            <hr style="width: 100%;margin:inherit;">
+        </div>
+
+
+    </div>
+
+    <div class="islerbox">
+        <div class="row">
+            <div class="span12">
+                <div class="islerbox">
+                    <div id="myCarousel" class="carousel slide">
+
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Carousel items -->
+                        <div class="carousel-inner">
+
+                            <div class="item active">
+                                <div class="row-fluid">
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                </div><!--/row-fluid-->
+                            </div><!--/item-->
+
+                            <div class="item  ">
+                                <div class="row-fluid">
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                </div><!--/row-fluid-->
+                            </div><!--/item-->
+                            <div class="item  ">
+                                <div class="row-fluid">
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                    <div class="col-lg-3 col-md-2 items item">
+                                        <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                                        <h2>Heading</h2>
+                                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                        <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+                                    </div><!-- /.col-lg-4 -->
+                                </div><!--/row-fluid-->
+                            </div><!--/item-->
+
+
+                        </div><!--/carousel-inner-->
+
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+                    </div><!--/myCarousel-->
+
+                </div><!--/well-->
+            </div>
+        </div>
+    </div>
+
+    <!----a1b-->
+    <div class="container-fluid">
+
+        <div class="islerbox">
+
+            <div style="text-align: center;" class="col-lg-3 col-md-4  items">
+                <div style="display: block;float: none;margin: auto;" class="c100 p65     green ">
+                    <span>Php</span>
+
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div> <h2>Heading</h2> </div>
+
+            <div style="text-align: center;" class="col-lg-3 col-md-4  items">
+                <div style="display: block;float: none;margin: auto;" class="c100 p65     orange ">
+                    <span>Php</span>
+
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div> <h2>Heading</h2> </div>
+            <div style="text-align: center;" class="col-lg-3 col-md-4  items">
+                <div style="display: block;float: none;margin: auto;" class="c100 p65      ">
+                    <span>Php</span>
+
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div> <h2>Heading</h2> </div>
+            <div style="text-align: center;" class="col-lg-3 col-md-4  items">
+                <div style="display: block;float: none;margin: auto;" class="c100 p65     orange ">
+                    <span>Php</span>
+
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div> <h2>Heading</h2> </div>
+        </div>
+    </div>
+    <!----a1b-->
+    <!---- icerik --->
+    <hr style="width: 100%;margin: 0px;s">
+    <div class="container-fluid">
+
+        <div class="islerbox">
+
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle " src="images/avatar3.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle" src="images/avatar3.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-3 col-md-2 items">
+                <img class="img-circle" src="images/avatar2.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                <p><a class="btn btn-default" href="#" role="button">Detaylar »</a></p>
+            </div><!-- /.col-lg-4 -->
+        </div>
+    </div>
+
+</div><!-- /.container -->
